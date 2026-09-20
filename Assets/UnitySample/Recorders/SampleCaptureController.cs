8d1c2fea263a537a53100e0d79225d57a4eff4a8
End3rbyte <end3rbyte@outlook.com>
2026-09-20 11:32:27 +0200

    Pass FFmpeg executable to recorder

--- Assets/UnitySample/Recorders/SampleCaptureController.cs
+++ Assets/UnitySample/Recorders/SampleCaptureController.cs
@@ -503,7 +503,7 @@ namespace UnityRuntimeCameraRecorder.Example
             _videoFiles.Add(Path.Combine(directory, $"{baseName}.mp4"));
             return new RecordingSettings
             {
-                FfmpegPath = _ffmpegDirectory,
+                FfmpegPath = Path.Combine(_ffmpegDirectory, "ffmpeg.exe"),
                 TemporaryContainerPath = Path.Combine(directory, $"{baseName}.mkv.tmp"),
                 GenerateStatistics = _generateStatistics,
                 OutputPath = Path.Combine(directory, $"{baseName}.mp4"),
